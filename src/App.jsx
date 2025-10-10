0b5d3feb3d66f8c9edf749f376056d9764e8ca51
ManiKumar Nakka <manikumarnakka324@gmail.com>
2025-10-10 11:59:29 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import ChatBox from "./components/ChatBox";
+import Community from "./pages/Community";
+import Credits from "./pages/Credits";
+
+const App = () => {
+  return (
+    <>
+      <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
+        <div className="flex h-screen w-screen">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<ChatBox />} />
+            <Route path="/community" element={<Community />} />
+            <Route path="/credits" element={<Credits />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
