2d145a82fa33119845f093568a081a8c0512790d
ManiKumar Nakka <manikumarnakka324@gmail.com>
2025-10-11 10:04:33 +0530

    sidebar component updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import moment from 'moment';
 
 const Sidebar = () => {
 
-  const {chats, setSelectedChat,theme, setTheme, user} = useAppContext();
+  const {chats, setSelectedChat,theme, setTheme, user,navigate} = useAppContext();
   const [search, setSearch] = useState('');
   return (
     <div className='flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from [#242124]/30 to-[#000000]/30 
@@ -46,6 +46,43 @@ const Sidebar = () => {
           </div>
         ))
         }
+      </div>
+
+      {/* Community Images */}
+      <div onClick={()=>navigate("/community")} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'>
+        <img src={assets.gallery_icon} alt="" className='w-4.5 not-dark:invert'/>
+        <div className='flex flex-col text-sm'>
+          <p>Community Images</p>
+        </div>
+      </div>
+
+      {/* Credits Purchase */}
+      <div onClick={()=>navigate("/credits")} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'>
+        <img src={assets.diamond_icon} alt="" className='w-4.5 dark:invert'/>
+        <div className='flex flex-col text-sm'>
+          <p>Credits : {user?.credits}</p>
+          <p className='text-sm text-gray-400'>Purchase credits to use QuickGPT</p>
+        </div>
+      </div>
+
+      {/* Dark Mode Toggle */}
+      <div className='flex items-center justify-between gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md '> 
+        <div className='flex items-center gap-2 text-sm'>
+          <img src={assets.theme_icon} alt="" className='w-4 not-dark:invert'/>
+          <p>Dark Mode</p>
+        </div>
+        <label className='relative inline-flex cursor-pointer'>
+          <input onChange={()=>setTheme(theme === "dark" ? "light" : "dark")} type="checkbox" className='sr-only peer' checked={theme === 'dark'}/>
+          <div className='w-9 h-5 bg-gray-400 rounded-full peer-checked:bg-purple-600 transition-all'></div>
+          <span className='absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform peer-checked:translate-x-4'></span>
+        </label>
+      </div>
+
+      {/* User Account */}
+      <div className='flex items-center gap-3 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer group'>
+        <img src={assets.user_icon} alt="" className='w-7 rounded-full'/>
+        <p className='flex-1 text-sm dark:text-primary truncate'>{user ? user.name : 'Login your account'}</p>
+        {user && <img src={assets.logout_icon} alt="" className='h-5 cursor-pointer hidden not-dark:invert group-hover:block'/>}
       </div>
     </div>
   )
